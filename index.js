025f74897e707b812649f809f4d756a11fcf8265
Daniel Ilkovich <daniel+github@betaworks.com>
2015-09-15 11:21:04 -0400

    separate outputs

--- index.js
+++ index.js
@@ -9,15 +9,24 @@ module.exports = {
       var postData = {
           token     : dexter.environment('slack_app_token') 
           , channel : step.input('channel_id').first()
-      }
-      , url = "https://slack.com/api/channels.history"
-      , self = this
+        }
+        , url = "https://slack.com/api/channels.history"
+        , self = this
       ;
 
       rest.post(url, { data: postData }).on('complete', function(result, response) {
-          return self.complete({
-              channel_history: result.messages
-          });
+          var data = {};
+
+          _.reduce(result.messages, function(result, message) {
+            _.each(['text', 'attachments', 'username', 'bot_id', 'icons.emoji', 'type', 'subtype', 'ts'], function(key) {
+                var val = _.get(message, key);
+                result[key] = result[key] || [];
+                result[key].push(val);
+            });
+            return result;
+          }, data);
+
+          return self.complete(data);
       });
   }
 };
